4e3fe0f915bb48893711dddb3f0c25464d61458a
TelloWick <fabriciovilca_2001@hotmail.com>
2026-05-09 19:04:59 -0500

    Modificacion de Main

--- app/src/main/java/com/example/lab08/MainActivity.kt
+++ app/src/main/java/com/example/lab08/MainActivity.kt
@@ -3,27 +3,38 @@ package com.example.lab08
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
-
+import com.example.lab08.data.TaskDatabase
+import com.example.lab08.ui.TaskScreen
 import com.example.lab08.ui.theme.Lab08Theme
-
-import kotlinx.coroutines.launch
+import com.example.lab08.viewmodel.TaskViewModel
+import com.example.lab08.viewmodel.TaskViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val database = Room.databaseBuilder(
+            applicationContext,
+            TaskDatabase::class.java,
+            "task_database"
+        ).build()
+
+        val dao = database.taskDao()
+
         setContent {
+
             Lab08Theme {
 
+                val viewModel: TaskViewModel = viewModel(
+                    factory = TaskViewModelFactory(dao)
+                )
+
+                TaskScreen(viewModel)
             }
         }
     }
 }
-
